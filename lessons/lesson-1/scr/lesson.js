ecee5fca9cfb4a945125e416c2def80c45a4c6cb
kkulikoff <62585869+kkulikoff@users.noreply.github.com>
2021-03-20 21:12:06 +0300

    Create lesson.js

---
+++ lessons/lesson-1/scr/lesson.js
@@ -0,0 +1,8 @@
+const student = 'Антон Куликов';
+const course = 'Базовый курс JavaScript';
+const lesson = '1';
+const task = '1.1 - 1.4' 
+document.getElementById("student").innerHTML = student;
+document.getElementById("course").innerHTML = course;
+document.getElementById("lesson").innerHTML = lesson;
+document.getElementById("task").innerHTML = task;
